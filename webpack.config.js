a27a9464a4e96e3bb6a316b7d936481af96cf1f1
Anya Bu <annabukhtiiarova@gmail.com>
2022-07-08 13:35:58 +0300

    webpack set corrected

--- webpack.config.js
+++ webpack.config.js
@@ -6,69 +6,62 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = function(env, argv) {
   console.log(`mode is: ${argv.mode}`);
 
-  if (argv.mode === 'development') {
-    config.mode = 'development'
-    config.devtool = 'source-map';
-  } else {
-    config.mode = 'production'
-  }
+  const isDevelopment = argv.mode === 'development';
 
-  return config
-}
-
-
-let config = {
-
-  // mode: argv.mode ? 'production' : 'development',
-  // devtool: argv.mode === "production" ? "source-map" : "eval",
-
-  entry: './src/index.js',
-  output: {
-    path: resolve(__dirname, 'build'),
-    filename: '[name]-[fullhash].js',
-    clean: true,
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ["@babel/plugin-transform-runtime"]
+  return {
+    entry: './src/index.js',
+    output: {
+      path: resolve(__dirname, 'build'),
+      filename: '[name]-[fullhash].js',
+      clean: true,
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ["@babel/plugin-transform-runtime"]
+            }
           }
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name]-[fullhash].css'
-    })
-  ],
-  devServer: {
-    port: 8080,
-    static: {
-      directory: join(__dirname, 'src')
-    }
-  },
-  performance : {
-    hints : false
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name]-[fullhash].css'
+      })
+    ],
+    devServer: {
+      port: 8080,
+      static: {
+        directory: join(__dirname, 'src')
+      }
+    },
+    performance: {
+      hints: false
+    },
+
+    devtool: isDevelopment
+      ? 'eval'
+      : 'source-map',
+
   }
 }
 
